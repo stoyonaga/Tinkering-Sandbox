c1eae3176adcd246af7e4b0ca5ff170dd0826ed7
stoyonaga <49795731+stoyonaga@users.noreply.github.com>
2022-08-05 11:39:38 -0400

    Create optimized_schema.sql

---
+++ SQL/Database Systems - The Complete Book (2nd)/Ch4/Examples/ex_1/study/optimized_schema.sql
@@ -0,0 +1,37 @@
+create table beers(
+    name varchar(25),
+    manf varchar(25),
+    primary key(name)
+);
+
+create table drinkers(
+    name varchar(25),
+    addr varchar(25),
+    beer_name varchar(25),
+    primary key(name),
+    foreign key(beer_name) references beers(name)
+);
+
+create table buddies(
+    buddy1 varchar(25),
+    buddy2 varchar(25),
+    primary key(buddy1, buddy2),
+    foreign key(buddy1) references drinkers(name),
+    foreign key(buddy2) references drinkers(name)
+);
+
+create table married(
+    husband varchar(25),
+    wife varchar(25),
+    primary key(husband, wife),
+    foreign key(husband) references drinkers(name),
+    foreign key(wife) references drinkers(name)
+);
+
+create table likes(
+    drinker varchar(25),
+    beer varchar(25),
+    primary key(drinker, beer),
+    foreign key(drinker) references drinkers(name),
+    foreign key(beer) references beers(name)
+);
