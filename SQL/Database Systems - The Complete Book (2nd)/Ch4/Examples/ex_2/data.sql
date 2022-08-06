bdb7b8102a0f1677bebdda3b0411c530b4075a55
stoyonaga <49795731+stoyonaga@users.noreply.github.com>
2022-08-06 11:24:44 -0400

    Create data.sql

---
+++ SQL/Database Systems - The Complete Book (2nd)/Ch4/Examples/ex_2/data.sql
@@ -0,0 +1,9 @@
+insert into beers (name, manf) values
+    ('Burton', 'Anheuser-Busch'),
+    ('Scotch', 'Anheuser-Busch'),
+    ('Gogo', 'Neutrog'),
+    ('Baja Blast', 'Mountain Dew');
+
+insert into ales(name, manf, colour) values
+    ('Burton', 'Anheuser-Busch', 'Brown'),
+    ('Scotch', 'Anheuser-Busch', 'Darkish Gold');
