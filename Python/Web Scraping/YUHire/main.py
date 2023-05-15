237c467d5f629a3e8cfbc707e01015391c185231
stoyonaga <49795731+stoyonaga@users.noreply.github.com>
2023-05-15 10:51:05 -0400

    Update main.py

--- Python/Web Scraping/YUHire/main.py
+++ Python/Web Scraping/YUHire/main.py
@@ -1,54 +1,43 @@
+from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
-import requests
+import time
 
-# Posting Object -- Used as a generalized container for the abstract object
-class Posting:
-    def __init__(self, faculty: str, campus: str, province: str):
-        self.faculty = faculty
-        self.campus = campus
-        self.province = province
+"""
+YU-Hire Search Automation 
+Last Checked: 2023-05-12
+"""
 
-    def summary(self) -> str:
-        return "Faculty / Affiliation: {}\nCampus Location: {}\nProvince: {}\n".format(
-            self.faculty, self.campus, self.province
-        )
+# Avoid Memory Leak (Close Operation Once Completed)
+with sync_playwright() as p:
+    # (chromium, firefox, or webkit are available browsers)
+    print("----- Browser Launched -----")
+    browser = p.chromium.launch(headless=True)
+    # Page Object (for interaction)
+    page = browser.new_page()
+    page.goto("https://jobs-ca.technomedia.com/yorkuniversity")
+    print ("----- Site Reached -----")
+    page.is_visible("#modal-content")
+    page.click("#btnCookieRejectALL")
+    page.click("#liNEWS_INTERNAL_JOBS")
+    page.get_by_title("Affiliation *").nth(1).click()
+    page.get_by_text("Summer Student").nth(1).click()
+    page.click("#btnSearchbutton2_1")
+    print("----- Query Launched -----")
+    # We sleep here to account for query update to reach the view (html) via controller (javascript) functions
+    time.sleep(3)
+    html = page.inner_html("#CTG_JOB_LIST")
+    print("----- Retrieving Data -----")
+    parser = BeautifulSoup(html, "html.parser")
+    rows = parser.find_all("tr")
+    print("----- Parsing Data -----")
+    for entry in rows:
+        try:
+            job_id = entry.find("td", {"class": "hidden-xs"}).text.strip()
+            posting_date = entry.find("td", {"tabindex" : "0"}).text.strip()
+            job_name = entry.find("div", {"class": "styleColLink"}).text.strip()
+            job_link = "https://jobs-ca.technomedia.com/yorkuniversity/" + entry.find("a", {"class": "relink"})["href"]
+            job_department = entry.find("div", {"class": "styleColAdd1"}).text.strip()
+            print("[ID: {}][Date Posted: {}]\nJob Name: {}\nDepartment: {}\n-- Application Link --\n{}\n".format(job_id, posting_date, job_name, job_department, job_link))
 
-
-print("===== YUHire Tool =====")
-# Scraping Parameters
-html_text = requests.get("https://jobs-ca.technomedia.com/yorkuniversity/")
-soup = BeautifulSoup(html_text.text, "lxml")
-jobs = soup.find_all("div", class_="col-sm-3 latestJobItems")
-# General Mapping for Filtering Purposes
-unique_entries = {}
-
-for job in jobs:
-    job_title = job.find("div", class_="col-xs-12 jobName").text.strip()
-    if job_title not in unique_entries.keys():
-        position = Posting(job.find("div", class_="col-xs-12 jobEntity").text.strip(),
-                           job.find("div", class_="col-xs-12 jobCity").text.strip(),
-                           job.find("div", class_="col-xs-12 jobCountry").text.strip())
-        unique_entries[job_title] = position
-
-print("Please enter a command sequence: ")
-print("1) View All Current Featured Jobs")
-print("2) View Jobs Based on a Keyword")
-
-try:
-    control_sequence = int(input())
-    if control_sequence == 1:
-        print("The current featured jobs are:\n")
-        for item in unique_entries.keys():
-            print(item)
-            print(unique_entries[item].summary())
-    elif control_sequence == 2:
-        keyword = input("Keyword:\n").lower()
-        for item in unique_entries.keys():
-            if keyword in item.lower():
-                print(item)
-                print(unique_entries[item].summary())
-except ValueError:
-    print("Incorrect value has been passed to the control sequence.")
-finally:
-    html_text.close()
-    quit(0)
+        except:
+            pass
